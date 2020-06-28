13358a208acc02a5713059f403d0d92aac729838
Manon Philippot <manon.philippot@gmail.com>
2020-06-28 16:18:44 +0200

    feat: DID-3 - add View Binding

--- app/src/main/java/com/example/didstock/MainActivity.java
+++ app/src/main/java/com/example/didstock/MainActivity.java
@@ -1,14 +1,17 @@
 package com.example.didstock;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import com.example.didstock.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
     }
 }
